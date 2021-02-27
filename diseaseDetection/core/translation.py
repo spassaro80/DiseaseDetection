87ef0d15184b69619512e14d473189ada4c07326
Stefano Passaro <stefano.passaro@gmail.com>
2021-02-27 19:17:25 +0100

    Finishing translation - Commit before database change

--- diseaseDetection/core/translation.py
+++ diseaseDetection/core/translation.py
@@ -1,5 +1,5 @@
 from modeltranslation.translator import translator, TranslationOptions
-from .models import Category ,Symptom, Disease
+from .models import Category ,Symptom, Disease, Survey
 
 class CategoryTranslationOptions(TranslationOptions):
     fields = ('name',)
@@ -10,6 +10,10 @@ class SymptomTranslationOptions(TranslationOptions):
 class DiseaseTranslationOptions(TranslationOptions):
     fields = ('name', 'description')
 
+class SurveyTranslationOptions(TranslationOptions):
+    fields = ('description',)
+
 translator.register(Category, CategoryTranslationOptions)
 translator.register(Symptom, SymptomTranslationOptions)
 translator.register(Disease, DiseaseTranslationOptions)
+translator.register(Survey, SurveyTranslationOptions)
